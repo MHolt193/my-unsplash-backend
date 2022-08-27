dbc0b449816cd86bcb5bf152e6788350a72f68b7
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-08-27 10:22:33 -0600

    remove cors package and multer

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv/config");
-const multer = require("multer");
-const cors = require('cors')
 const port = process.env.PORT || 5000;
 const connectDb = require("./config/db");
 
@@ -14,7 +12,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //cors all origins
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Controll-Allow-Credentials", true)
+  next();
+});
 
 //Routes
 app.use('/api/images', require("./routes/imageRoutes"))
